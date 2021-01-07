20ea56ab41ce982c315a6eed440e2fb726a15269
austintip <austin.d.tipograph@gmail.com>
2021-01-07 13:47:51 -0500

    have functions working but giving back results of NaN?

--- js/main.js
+++ js/main.js
@@ -5,21 +5,32 @@ let minusBtn = document.getElementById('minus')
 let numberInput = document.getElementById('number-input')
 
 
-//make numberDisplay 0 on page load.
-document.addEventListener('DOMContentLoaded', (e => {
-    numberDisplay.innerText = "0"
-}));
+// make numberDisplay 0 on page load.
+// document.addEventListener('DOMContentLoaded', (e => {
+//     numberDisplay.innerText = 0
+// }));
 
 
+//make a function that takes input and adds it to numberDisplay with plus then changes innertext
+function add() {
+    var one = parseFloat(numberInput.value);
+    var two = parseFloat(numberDisplay.value);
+    numberDisplay.innerText = one+two;
+}
+//make function that takes input and subtracts it from numberDisplay with minus then changes innertext 
+function subtract() {
+    var one = parseFloat(numberInput.value);
+    var two = parseFloat(numberDisplay.value);
+    numberDisplay.innerText = one-two;
+}
 //make an event listener for the plus click - call add function in it
+
 plusBtn.addEventListener('click', (event => {
-    console.log('clicked plus')
+    console.log('is it working?')
+    add();
 }));
 //make an event listener for minus click - call minus function in it
 minusBtn.addEventListener('click', (event => {
     console.log('clicked minus')
+    subtract();
 }));
-
-
-//make a function that takes input and adds it to numberDisplay with plus then changes innertext
-//make function that takes input and subtracts it from numberDisplay with minus then changes innertext 
