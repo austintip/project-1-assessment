9bb5329cc0d796bcb1995bfa972e390d45a22f43
austintip <austin.d.tipograph@gmail.com>
2021-01-07 21:33:59 -0500

    working now with colors too

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
 // select all html elements
-let numberDisplay = document.getElementById('number-display');
-let plusBtn = document.getElementById('plus');
-let minusBtn = document.getElementById('minus');
-let numberInput = document.getElementById('number-input');
+const numberDisplay = document.getElementById('number-display');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+const numberInput = document.getElementById('number-input');
 
-
-// make numberDisplay 0 on page load.
-document.addEventListener('DOMContentLoaded', (e => {
-    numberDisplay.innerText = "0"
-}));
-
+let num = 0;
 
 //make a function that takes input and adds it to numberDisplay with plus then changes innertext
-function add() {
-    var one = numberInput;
-    var two = parseInt(numberDisplay);
-    numberDisplay.innerText = one+two;
+function add(e) {
+    e.preventDefault();
+    num += parseInt(numberInput.value);
+    numberDisplay.innerText = num;
+    if (num >= 0) {
+        numberDisplay.style.color = "black";
+    }
 }
 //make function that takes input and subtracts it from numberDisplay with minus then changes innertext 
-function subtract() {
-    var one = parseInt(numberInput)
-    var two = parseInt(numberDisplay)
-    numberDisplay.innerText = two-one;
-}
+function subtract(e) {
+    e.preventDefault();
+    num -= parseInt(numberInput.value);
+    numberDisplay.innerText = num;
+    if (num < 0) {
+        numberDisplay.style.color = "red";
+    }
+};
 //make an event listener for the plus click - call add function in it
 
-plusBtn.addEventListener('click', (event => {
-    add();
-}));
+plusBtn.addEventListener('click', (e) => {
+    add(e);
+    console.log(numberInput)
+    console.log(numberDisplay)
+});
 //make an event listener for minus click - call minus function in it
-minusBtn.addEventListener('click', (event => {
-    subtract();
-    if (numberDisplay < 0) {
-        numberDisplay.style.color = red
-    }
-}));
+minusBtn.addEventListener('click', (e) => {
+    subtract(e);
+    console.log(numberInput)
+    console.log(numberDisplay)
+});
+
+
